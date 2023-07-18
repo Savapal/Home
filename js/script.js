1135a6598a9249565b0d7e37fb48f3517c6a63e3
Savapal <sava_pl@bk.ru>
2023-07-18 21:40:30 +0300

    summary
    summary

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+const animItems = document.querySelectorAll(".anim-items");
+
+if (animItems.length > 0) {
+    function animOnScrjll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset =
+        }
+    }
+    function ofset(el) {
+        const rcet =el.getBoundingClientRect(),
+        scrollleft =...
+    }
+}
